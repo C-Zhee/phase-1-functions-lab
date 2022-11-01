98384ae0627f3c870ebe57b4f2dc3d03a50ef8b9
Corey <coreyzhee@gmail.com>
2022-11-01 00:16:52 -0400

    commit tree 04c1a266c1e2f3d509192da0ac771b6ca8a2d84a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(int){
+    return Math.abs(42 - int);
+};
+
+function distanceFromHqInFeet(int){
+    return Math.abs(264 * int)
+};
+
+function distanceTravelledInFeet(int){
+    return Math.abs (264 - int)
+};
